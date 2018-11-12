e00bf4a9a475690ebe95f6bbbcd1d47ce7d78883
Peter Bindels <peter.bindels@tomtom.com>
2018-11-12 20:31:01 +0100

    Add error_code alias to filesystem header

--- fw/include/fw/filesystem.hpp
+++ fw/include/fw/filesystem.hpp
@@ -3,7 +3,9 @@
 #if 1
 #    include <boost/filesystem.hpp>
 namespace filesystem = boost::filesystem;
+using error_code = boost::system::error_code;
 #else
 #    include <filesystem>
 namespace filesystem = std::filesystem;
+using error_code = std::error_code;
 #endif
