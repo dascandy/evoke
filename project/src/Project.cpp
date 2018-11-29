7865a2bd7e4ffb1bf6ac6f93eefa8994bd975e19
Peter Bindels <peter.bindels@tomtom.com>
2018-11-29 23:54:29 +0100

    Fix minor bug in Project.cpp that caused Windows to find only empty components

--- project/src/Project.cpp
+++ project/src/Project.cpp
@@ -104,6 +104,7 @@ std::ostream &operator<<(std::ostream &os, const Project &p)
 void Project::ReadCode(std::unordered_map<std::string, File> &files, const filesystem::path &path, Component &comp)
 {
     File &f = files.emplace(path.generic_string().substr(2), File(path.generic_string().substr(2), comp)).first->second;
+    comp.files.insert(&f);
 #ifdef _WIN32
     std::string buffer;
     buffer.resize(filesystem::file_size(path));
@@ -112,7 +113,6 @@ void Project::ReadCode(std::unordered_map<std::string, File> &files, const files
     }
     ReadCodeFrom(f, buffer.data(), buffer.size());
 #else
-    comp.files.insert(&f);
     int fd = open(path.c_str(), O_RDONLY);
     size_t fileSize = filesystem::file_size(path);
     void *p = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
