b3ea23f7fc07eedaa14238474451ae0b9a0cfd1e
anthonyhl <anthony@ahuangliang.cn>
2019-05-17 01:38:21 +0800

    fixed typo

--- project/test/main.cpp
+++ project/test/main.cpp
@@ -6,9 +6,16 @@
 
 namespace btt = boost::test_tools;
 
-BOOST_AUTO_TEST_CASE(without_last_flash)
+BOOST_AUTO_TEST_CASE(without_last_slash)
 {
     Component c("./my/component", true);
+
+    BOOST_TEST(c.name == "my_component");
+}
+
+BOOST_AUTO_TEST_CASE(with_last_slash)
+{
+    Component c("./my/component/", true);
 
     BOOST_TEST(c.name == "my_component");
 }
