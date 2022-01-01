1d6a8f33f82e692aa46557ccaec0275b1bb94e6a
Peter Bindels <dascandy@gmail.com>
2022-01-01 22:38:10 +0100

    Make include guard match the implementation guard

--- fw/src/sha512.cpp
+++ fw/src/sha512.cpp
@@ -14,19 +14,19 @@
 std::array<uint8_t, 64> sha512(const fs::path& path) 
 {
     std::array<uint8_t, 64> hash;
-#if defined(_MSC_VER)
-    std::ifstream inFile(path);
-    auto filesize = fs::file_size(path);
-    auto target = std::make_unique<std::uint8_t[]>(filesize);
-    inFile.read(reinterpret_cast<char*>(target.get()), filesize);
-    SHA512(target.get(), filesize, hash.data());
-#else
+#if __linux__
     int fd = open(path.c_str(), O_RDONLY);
     size_t filesize = fs::file_size(path);
     void* target = mmap(nullptr, filesize, PROT_READ, MAP_SHARED, fd, 0);
     close(fd);
     SHA512((const unsigned char*)target, filesize, hash.data());
     munmap(target, filesize);
+#else
+    std::ifstream inFile(path);
+    auto filesize = fs::file_size(path);
+    auto target = std::make_unique<std::uint8_t[]>(filesize);
+    inFile.read(reinterpret_cast<char*>(target.get()), filesize);
+    SHA512(target.get(), filesize, hash.data());
 #endif
     return hash;
 }
