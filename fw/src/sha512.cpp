ba1146b4e45160561bb3576e2d580cfff8104336
Peter Bindels <dascandy@gmail.com>
2022-07-11 08:58:14 +0200

    fix crash for zero-sized files

--- fw/src/sha512.cpp
+++ fw/src/sha512.cpp
@@ -6,11 +6,15 @@
 
 std::array<uint8_t, 64> sha512(const fs::path& path) 
 {
-    std::array<uint8_t, 64> hash;
-    using namespace boost::interprocess;
-    file_mapping file(path.string().c_str(), read_only);
-    mapped_region region(file, read_only);
-    SHA512(static_cast<unsigned char *>(region.get_address()), region.get_size(), hash.data());
+    std::array<uint8_t, 64> hash = {};
+    size_t fileSize = fs::file_size(path.string());
+    if(fileSize > 0)
+    {
+        using namespace boost::interprocess;
+        file_mapping file(path.string().c_str(), read_only);
+        mapped_region region(file, read_only);
+        SHA512(static_cast<unsigned char *>(region.get_address()), region.get_size(), hash.data());
+    }
     return hash;
 }
 
